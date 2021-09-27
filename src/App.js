ed1543940a5b7067093669b8961e3e6ec1554558
Ivan Epou <ivanepou92@gmail.com>
2021-09-27 12:34:09 +0300

    feat: remove comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import Home from "./Page/Home"
 import Nav from "./Componets/Nav"
 import Footer from "./Componets/Footer"
@@ -11,12 +11,12 @@ function App() {
     <div className="App">
       <Router>
         <Nav/>
-        {/* <Switch> */}
+        <Switch>
           {/* <Route exact path="/product"  component={Product} /> */}
           {/* <Route exact path="/"  component={Home} /> */}
           <Route exact path="/" render={() => <Home />} />
           <Route  path="/product" render={() => <Product />} />
-        {/* </Switch> */}
+        </Switch>
         {/* <Home/> */}
     {/* <Product /> */}
         <Footer/>
